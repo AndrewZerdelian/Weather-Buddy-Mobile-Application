ebfd4df527028085e54b3b6fd8effdc98f05198b
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-03-31 16:35:21 +0200

    First-Reusable-Component-Done-And-App-Name-And-Logo

--- App/Reusable-Components/CurrentWeather.jsx
+++ App/Reusable-Components/CurrentWeather.jsx
@@ -1,26 +1,54 @@
 import { View, Text } from "react-native";
 import { Image } from "react-native";
 import React from "react";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { EvilIcons } from "@expo/vector-icons";
-export default function CurrentWeather() {
+
+export default function CurrentWeather({
+  Current_Weather,
+  temp,
+  Weather_Description,
+  Weather_Icon,
+  name,
+  country,
+  feels_like,
+  temp_max,
+  temp_min,
+}) {
   return (
     <View className="p-5">
       <View className="bg-[#1C1C1E] rounded-2xl p-5">
-        <Text className="text-white text-xl font-bold">Current Weather</Text>
-        <View className="pt-5">
-          <Text className="text-white text-2xl font-bold">31°C</Text>
-          <Image src="../../assets/splash.png" alt="Weather-Icon" />
+        <Text className="text-white text-xl font-bold">{Current_Weather}</Text>
+        <View className="pt-5 flex flex-row justify-between">
+          <View>
+            <Text className="text-white text-6xl font-bold">{temp}°</Text>
+            <Text className="text-white font-bold">
+              Hight {temp_max}° / Low {temp_min}°
+            </Text>
+          </View>
+          <View>
+            <Image
+              source={{
+                uri: `https://openweathermap.org/img/wn/${Weather_Icon}@2x.png`,
+              }}
+              alt="Weather-Icon"
+              style={{ width: 100, height: 100 }}
+            />
+            <Text className="text-white text-center font-bold">
+              Feels like {feels_like}°
+            </Text>
+          </View>
         </View>
-        <Text className="text-white font-bold border-b border-b-slate-100 pt-5">
-          Few Clouds
+        <Text className="text-white font-bold  pt-5">
+          {Weather_Description}
         </Text>
+        <Text className="border-b border-b-slate-100"></Text>
         <View className="flex flex-row pt-5">
           <EvilIcons name="location" size={20} color="white" />
-          <Text className="text-white font-bold ">Cairo Governorate / EG</Text>
+          <Text className="text-white font-bold ">
+            {name} / {country}
+          </Text>
         </View>
       </View>
     </View>
   );
 }
-//style={{ color: "white", flex: 1, justifyContent: "center" }}
--- App/Weather_Condition.jsx
+++ App/Weather_Condition.jsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
+import React, { useContext } from "react";
 import CurrentWeather from "./Reusable-Components/CurrentWeather";
+import { SearchByLocation } from "./Context/Location_Context";
 
 export default function Weather_Condition() {
+  const { ForcastByLocation } = useContext(SearchByLocation);
+  //console.log(ForcastByLocation?.data);
+  console.log(ForcastByLocation?.data?.list[0]);
   return (
-    <View >
-      <CurrentWeather/>
+    <View>
+      <CurrentWeather
+        Current_Weather={"Current Weather"}
+        name={ForcastByLocation?.data?.city?.name}
+        country={ForcastByLocation?.data?.city?.country}
+        temp={ForcastByLocation?.data?.list[0]?.main?.temp.toFixed(0)}
+        Weather_Description={
+          ForcastByLocation?.data?.list[0]?.weather[0]?.description
+        }
+        Weather_Icon={ForcastByLocation?.data?.list[0]?.weather[0]?.icon}
+        feels_like={ForcastByLocation?.data?.list[0]?.main?.feels_like.toFixed(
+          0
+        )}
+        temp_max={ForcastByLocation?.data?.list[0]?.main?.temp_max.toFixed(0)}
+        temp_min={ForcastByLocation?.data?.list[0]?.main?.temp_min.toFixed(0)}
+      />
     </View>
   );
 }
