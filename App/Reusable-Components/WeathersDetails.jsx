cccbb357a7526c25700baf10d9c2490d9a176737
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-04-01 17:54:12 +0200

    first update

---
+++ App/Reusable-Components/WeathersDetails.jsx
@@ -0,0 +1,71 @@
+import { View, Text, ScrollView } from "react-native";
+import React, { useContext } from "react";
+import { Image } from "react-native";
+import { SearchByLocation } from "../Context/Location_Context";
+
+export default function WeathersDetails({ TimeIn12Hour, Icon, Temp }) {
+  return (
+    <View className="pt-3 ">
+      <Text className="text-white font-bold text-center">{TimeIn12Hour}</Text>
+      <Image
+        source={{
+          uri: `https://openweathermap.org/img/wn/${Icon}@2x.png`,
+        }}
+        alt="Weather-Icon"
+        style={{ width: 75, height: 75 }}
+      />
+      <Text className="text-white font-bold text-center">
+        {Temp.toFixed(0)}°
+      </Text>
+    </View>
+  );
+}
+
+/** Horizontal SCROLLING WORKING FINE this option is not a reuble component
+ * 
+ * import { View, Text, ScrollView } from "react-native";
+import React, { useContext } from "react";
+import { Image } from "react-native";
+import { SearchByLocation } from "../Context/Location_Context";
+
+export default function WeathersDetails({ Time, Temp, Weather_Icon }) {
+  const { ForcastByLocation } = useContext(SearchByLocation);
+  console.log(ForcastByLocation?.data?.list);
+
+  return (
+    <View View className="p-5">
+      <View className="bg-[#1C1C1E] rounded-2xl p-5">
+        <Text className="text-white text-xl font-bold">Weathers Details</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {ForcastByLocation?.data?.list?.slice(0, 10)?.map((items) => {
+            // Convert the 24-hour format to 12-hour format
+            const date = new Date(items.dt_txt);
+            const timeIn12Hour = date.toLocaleTimeString("en-US", {
+              hour: "numeric",
+              hour12: true,
+            });
+            return (
+              <View className="pt-3">
+                <Text className="text-white font-bold text-center">
+                  {timeIn12Hour}
+                </Text>
+                <Image
+                  source={{
+                    uri: `https://openweathermap.org/img/wn/${items?.weather[0]?.icon}@2x.png`,
+                  }}
+                  alt="Weather-Icon"
+                  style={{ width: 75, height: 75 }}
+                />
+                <Text className="text-white font-bold text-center">
+                  {items?.main.temp.toFixed(0)}°
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+ * 
+ */
--- App/Weather_Condition.jsx
+++ App/Weather_Condition.jsx
@@ -1,12 +1,13 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import React, { useContext } from "react";
 import CurrentWeather from "./Reusable-Components/CurrentWeather";
 import { SearchByLocation } from "./Context/Location_Context";
+import WeathersDetails from "./Reusable-Components/WeathersDetails";
 
 export default function Weather_Condition() {
   const { ForcastByLocation } = useContext(SearchByLocation);
   //console.log(ForcastByLocation?.data);
-  console.log(ForcastByLocation?.data?.list[0]);
+
   return (
     <View>
       <CurrentWeather
@@ -24,6 +25,34 @@ export default function Weather_Condition() {
         temp_max={ForcastByLocation?.data?.list[0]?.main?.temp_max.toFixed(0)}
         temp_min={ForcastByLocation?.data?.list[0]?.main?.temp_min.toFixed(0)}
       />
+      <View className="p-5">
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          className="py-5 bg-[#1C1C1E] rounded-2xl"
+        >
+          {ForcastByLocation?.data?.list?.slice(0, 10)?.map((items) => {
+            // Convert the 24-hour format to 12-hour format
+            const date = new Date(items.dt_txt);
+            const timeIn12Hour = date.toLocaleTimeString("en-US", {
+              hour: "numeric",
+              hour12: true,
+            });
+            return (
+              <WeathersDetails
+                TimeIn12Hour={timeIn12Hour}
+                Icon={items?.weather[0]?.icon}
+                Temp={items?.main.temp}
+              />
+            );
+          })}
+        </ScrollView>
+      </View>
     </View>
   );
 }
+/**
+ * Time={ForcastByLocation?.data?.list?.dt}
+        Weather_Icon={ForcastByLocation?.data?.list[0]?.weather[0]?.icon}
+        Temp={ForcastByLocation?.data?.list[0]?.main?.temp.toFixed(0)}
+ */
