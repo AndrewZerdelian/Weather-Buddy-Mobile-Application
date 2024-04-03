82a64cbada318f1095cafd3dc75e6146c35ca145
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-04-03 12:33:40 +0200

    update

--- App/Screens/Searched_Weather_Condition.jsx
+++ App/Screens/Searched_Weather_Condition.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import React from "react";
 
 export default function Searched_Weather_Condition() {
   return (
     <View>
-      <Text>Searched_Weather_Condition</Text>
+      <Text>Hello there</Text>
     </View>
   );
 }
+/**
+ *     <SafeAreaView>
+      <Text>Searched_Weather_Condition</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+ */
